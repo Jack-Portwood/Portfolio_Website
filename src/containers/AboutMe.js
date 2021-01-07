a04ff18a221e064bbf7e0f317eabf904baec68c1
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-07 15:59:47 +0000

    flip card responding

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -6,21 +6,24 @@ const AboutMe = () => {
   //this will contain a styled card containing text
 
   return (
-      <Parallax
-        className="parallax-container"
-        bgImage={banner}
-        strength={500}
-        blur={{ min: -1, max: 1 }}
-      >
-        <div className="aboutMe-container">
+    <Parallax
+      className="parallax-container"
+      bgImage={banner}
+      strength={500}
+      blur={{ min: -1, max: 1 }}
+    >
+      <div className="aboutMe-container">
         <div className="card">
-          <div className="card-front"> About Me </div>
+          <div className="card-front">
+            <div className="text-front-card"> About Me</div>
+          </div>
+
           <div className="card-back">
             <div className="text-back-card"> Live. Laugh. Love.</div>
           </div>
-          </div>
         </div>
-      </Parallax>
+      </div>
+    </Parallax>
   );
 };
 
