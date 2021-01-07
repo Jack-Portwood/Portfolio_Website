e6caa8c27b8376583e75b2e2344b9e570cec5dd3
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-07 14:57:08 +0000

    parallax and flip card formatted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./index.scss";
+import'./index.scss'
 import MainContainer from "./containers/MainContainer";
 
 const App = () => {
--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import {Parallax} from "react-parallax"
-import banner from '../banner.jpg'
-
+import React from "react";
+import { Parallax } from "react-parallax";
+import banner from "../banner.jpg";
 
 const AboutMe = () => {
+  //this will contain a styled card containing text
 
-    //this will contain a styled card containing text 
-
-    return (
-      <Parallax bgImage={banner} strength={500} blur={{ min: -1, max: 3 }}>
+  return (
+      <Parallax
+        className="parallax-container"
+        bgImage={banner}
+        strength={500}
+        blur={{ min: -1, max: 1 }}
+      >
         <div className="aboutMe-container">
-          <div className="card">
-            <div className="card-front"> About Me </div>
-            <div className="card-back">
-              <div className="text-back-card"> Live. Laugh. Love.</div>
-            </div>
+        <div className="card">
+          <div className="card-front"> About Me </div>
+          <div className="card-back">
+            <div className="text-back-card"> Live. Laugh. Love.</div>
+          </div>
           </div>
         </div>
       </Parallax>
-    );
-}
+  );
+};
 
 export default AboutMe;
