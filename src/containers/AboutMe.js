d0add352058bdd612c5175303707f9c4b571be2b
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-07 12:59:43 +0000

    scaling fix for home, navbar

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Parallax} from "react-parallax"
-// import banner from '../banner.jpg'
+import banner from '../banner.jpg'
 
 
 const AboutMe = () => {
@@ -8,17 +8,16 @@ const AboutMe = () => {
     //this will contain a styled card containing text 
 
     return (
-      // <Parallax bgImage={banner}>
-      <div className="aboutMe-container">
-        <div className="banner"></div>
-        <div className="card">
-          <div className="card-front"> About Me </div>
-          <div className="card-back">
-            <div className="text-back-card"> Live. Laugh. Love.</div>
+      <Parallax bgImage={banner} strength={500} blur={{ min: -1, max: 3 }}>
+        <div className="aboutMe-container">
+          <div className="card">
+            <div className="card-front"> About Me </div>
+            <div className="card-back">
+              <div className="text-back-card"> Live. Laugh. Love.</div>
+            </div>
           </div>
         </div>
-      </div>
-      //  </Parallax>
+      </Parallax>
     );
 }
 
