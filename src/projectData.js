34ac24597a231a63e2118f3222171a615d0e8d4d
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-08 09:08:03 +0000

    descriptions and titles formatted

--- src/projectData.js
+++ src/projectData.js
@@ -1,19 +1,23 @@
 export default [
   {
     id: 1,
-    projectName: "Scroller",
+    title: "Scroller",
     description:
-      "Scroller was envisioned as a fun app to allow easy communication when speaking wasn't feasible. Use cases envisioned where at loud events such as nightclubs or concerts, moderate distances and situations where small text was not feasible, perhaps aiding the visually impaired.",
+      "Scroller was envisioned as a fun app to allow easy communication when speaking wasn't feasible.",
     image: "images/scroller.png",
   },
   {
     id: 2,
-    projectName: "Countdown Timer",
+    title: "NYE Countdown",
     description:
-      "This appilcation will count down till 00:00:00 31/12/20 when it will render a new background image and text. The main aim of this project was allow me to become familiar with React Hooks (useState, useEffect and useRef) and to gain understanding how time worked in ReactJS.",
+      "This appilcation will count down till 00:00:00 when it will render a new background image and text. ",
     image: "images/timer.png",
   },
-  { id: 3, projectName: "Request Dugs", 
-    description: "Request-Dugs was initially an excerise in refamiliarize myself with async fetch requests. Although is app has a simple scope it provides a gentle lift during the harder days. Please take some time today to Request Dugs.", 
-    image: "images/dugs.png" },
+  {
+    id: 3,
+    title: "Request Dugs",
+    description:
+      "Request-Dugs was initially an excerise in refamiliarize myself with async fetch requests. ",
+    image: "images/dugs.png",
+  },
 ];
