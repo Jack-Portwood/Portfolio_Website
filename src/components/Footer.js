b0ce01e578a9b071908f5875097b2154abd12b8d
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-10 16:05:28 +0000

    social icons postioned

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import LogoB from '../LogoB.png'
+import React from "react";
+import LogoB from "../LogoB.png";
 const Footer = () => {
+  //This will preasent Logo and socail media links
 
-    //This will preasent Logo and socail media links
-
-    return (
-      <div className="footer-container">
-        
+  return (
+    <div className="footer-container">
+      <div className="social-media">
+        <i class="fab fa-linkedin-in"></i>
         <img src={LogoB} className="logoB" alt="Jack Portwood Logo" />
-      </div>
-    );
-}
 
-export default Footer 
+        <i class="fab fa-github"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
