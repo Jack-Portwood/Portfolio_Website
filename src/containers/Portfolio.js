60b12c2be6b558e303304e6b519ec95124de5b8b
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-10 11:48:17 +0000

    footer scaled

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import projectData from '../projectData'
+import projectData from "../projectData";
 import Projects from "../components/Projects";
 import { Parallax } from "react-parallax";
-import banner2 from '../banner2.jpg'
+import banner2 from "../banner2.jpg";
 // import banner from '../banner.jpg'
 
 const Portfolio = () => {
   //This will container each indivdual project like a list
-
-  
 
   const [portfolioData, setPortfolioData] = useState(projectData);
 
@@ -23,7 +21,7 @@ const Portfolio = () => {
       strength={500}
       blur={{ min: -1, max: 1 }}
     >
-      <div className="portfolio-container" id={"portfolio-container"}>
+      <div className="portfolio-container" id="portfolio-container">
         {data}
       </div>
     </Parallax>
