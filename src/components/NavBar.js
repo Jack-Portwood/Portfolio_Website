70df65899b7884f3994b7f3d157f341f697fcf43
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-11 16:29:01 +0000

    refactored with hooks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,16 @@
-import React,{Component} from "react";
+import React,{useState} from "react";
 import { HashLink } from "react-router-hash-link";
 
-class NavBar extends Component {
-  state = {
-    isOpen: false
+const NavBar = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+  
+    function handleTogle() { 
+    setIsOpen({ isOpen: !setIsOpen})
   }
-  handleToggle = () =>{
-    this.setState({ isOpen: !this.state.isOpen})
-  }
-  render(){
   return (
     <nav className="navbar">
-      <ul className={this.state.isOpen ? "show-nav" : "nav-links"}>
+      <ul className={isOpen ? "show-nav" : "nav-links"}>
         <li>
           <HashLink smooth to="#home-container" className="nav-link">
             Home
@@ -31,5 +30,42 @@ class NavBar extends Component {
     </nav>
   );
 };
-}
+
 export default NavBar;
+
+// import React, { Component } from "react";
+// import { HashLink } from "react-router-hash-link";
+
+// class NavBar extends Component {
+//   state = {
+//     isOpen: false,
+//   };
+//   handleToggle = () => {
+//     this.setState({ isOpen: !this.state.isOpen });
+//   };
+//   render() {
+//     return (
+//       <nav className="navbar">
+//         <ul className={this.state.isOpen ? "show-nav" : "nav-links"}>
+//           <li>
+//             <HashLink smooth to="#home-container" className="nav-link">
+//               Home
+//             </HashLink>
+//           </li>
+//           <li>
+//             <HashLink smooth to="#aboutMe-container" className="nav-link">
+//               About
+//             </HashLink>
+//           </li>
+//           <li>
+//             <HashLink smooth to="#portfolio-container" className="nav-link">
+//               Projects
+//             </HashLink>
+//           </li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+// export default NavBar;
+
