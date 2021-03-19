ba52cc0290dd2878025e7cb485fd0ba0c927e211
Jack Portwood <Jack.Portwood14@gmail.com>
2021-03-19 10:59:55 +0000

    navBar updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,9 +6,13 @@ const NavBar = () => {
 
   const [isOpen, setIsOpen] = useState(false)
   
-    function handleTogle() { 
-    setIsOpen({ isOpen: !setIsOpen})
-  }
+
+  //Started to build out function for hamburger might need its own component
+  //   function handleTogle() { 
+  //   setIsOpen({ isOpen: !setIsOpen})
+  // }
+
+
   return (
     <nav className="navbar">
       <ul className={isOpen ? "show-nav" : "nav-links"}>
@@ -34,39 +38,5 @@ const NavBar = () => {
 
 export default NavBar;
 
-// import React, { Component } from "react";
-// import { HashLink } from "react-router-hash-link";
 
-// class NavBar extends Component {
-//   state = {
-//     isOpen: false,
-//   };
-//   handleToggle = () => {
-//     this.setState({ isOpen: !this.state.isOpen });
-//   };
-//   render() {
-//     return (
-//       <nav className="navbar">
-//         <ul className={this.state.isOpen ? "show-nav" : "nav-links"}>
-//           <li>
-//             <HashLink smooth to="#home-container" className="nav-link">
-//               Home
-//             </HashLink>
-//           </li>
-//           <li>
-//             <HashLink smooth to="#aboutMe-container" className="nav-link">
-//               About
-//             </HashLink>
-//           </li>
-//           <li>
-//             <HashLink smooth to="#portfolio-container" className="nav-link">
-//               Projects
-//             </HashLink>
-//           </li>
-//         </ul>
-//       </nav>
-//     );
-//   }
-// }
-// export default NavBar;
 
